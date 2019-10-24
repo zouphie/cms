88d555d7b2e5e2db83d4de2b01b019ff45b5d9f7
Simon Seebacher <sseebacher.mmt-b2018@fh-salzburg.ac.at>
2019-10-24 11:28:45 +0200

    add burger menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./sass/styles.scss";
+
+const nav = document.querySelector("header nav");
+const burger = document.querySelector(".burger");
+
+function toggleNav() {
+    nav.classList.toggle("nav-active");
+    burger.classList.toggle("nav-active");
+}
+
+burger.addEventListener("click", toggleNav);
