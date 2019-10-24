ff4b9f2abba53746ce2a7df3846abd7b2d03a993
Simon Seebacher <sseebacher.mmt-b2018@fh-salzburg.ac.at>
2019-10-24 09:31:25 +0200

    add html-loader and change url-loader to file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "dist")
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(gif|jpg|png)$/i,
-          use: {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
-            }
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader
-            },
-            'css-loader',
-            'sass-loader',
-          ],
-        },
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        inject: true
-      }),
-      new MiniCssExtractPlugin({
-        filename: './styles.css'
-      }),
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      }
     ]
-  };
-  
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: './styles.css'
+    }),
+  ]
+};
