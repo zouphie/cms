027b021639168a72d0e96f283d32ff04c7289bfa
Sophie Leitner <36165649+zouphie@users.noreply.github.com>
2022-05-31 14:32:44 +0200

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'db_alex' );
 define( 'DB_USER', 'dbuser_alex' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Das1557939!' );
+define( 'DB_PASSWORD', 'xxxxxx' );
 
 /** MySQL hostname */
 define( 'DB_HOST', '127.0.0.1' );
